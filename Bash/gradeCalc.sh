8d9ccea36498bc6c03746b44c8c6314b9e08f866
Ikaros <tudormateian@gmail.com>
2023-02-25 16:47:12 +0200

    bash changes

--- Bash/gradeCalc.sh
+++ Bash/gradeCalc.sh
@@ -10,7 +10,6 @@ SDA=9
 TGC=10
 
 credit3=$((5*TGC + 5*SO1 + 5*BD1 + 5*P3 + 4*SDA + 2*PI + 2*EN3 + 2*CA))
-echo $(bc -l <<< "$credit3/30")
 medie3=$(bc -l <<< "$credit3/30")
 
-echo "medie ponderata: $medie3"
+printf "%d/300: %.3f" "$credit3" "$medie3"
