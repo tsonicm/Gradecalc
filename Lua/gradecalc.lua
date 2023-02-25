62db058a04673c16412b5dacabe2ef0c5e9bfa8c
Ikaros <tudormateian@gmail.com>
2023-02-26 01:27:40 +0200

    Added Lua

---
+++ Lua/gradecalc.lua
@@ -0,0 +1,13 @@
+BD1 = 7
+CA  = 10
+EN3 = 9
+P3  = 8
+PI  = 9
+SO1 = 8
+SDA = 9
+TGC = 10
+
+credit3 = 5*TGC + 5*SO1 + 5*BD1 + 5*P3 + 4*SDA + 2*PI + 2*EN3 + 2*CA
+medie3 = credit3 / 30
+
+print(tostring(credit3) .. "/300: " .. string.format("%.3f", medie3))
