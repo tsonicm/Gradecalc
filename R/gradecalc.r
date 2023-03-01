e331e545d7d3778d8969088e9f710248498e26db
Ikaros <tudormateian@gmail.com>
2023-03-01 09:28:53 +0200

    Added R

---
+++ R/gradecalc.r
@@ -0,0 +1,13 @@
+bd1 <- 7
+ca <- 10
+en3 <- 9
+p3 <- 8
+pi <- 9
+so1 <- 8
+sda <- 9
+tgc <- 10
+
+credit3 <- 5 * tgc + 5 * so1 + 5 * bd1 + 5 * p3 + 4 * sda + 2 * pi + 2 * en3 + 2 * ca
+medie3 <- credit3 / 30
+
+print(paste(credit3, "/300: ", format(round(medie3, 3), nsmall = 3)))
